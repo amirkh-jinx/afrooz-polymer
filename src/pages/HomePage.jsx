daf486359946ad7bd76a779be77209dccd9778ea
amirkh-jinx <amirkheirabadi2008@gmail.com>
2026-02-25 04:00:34 +0330

    landing part 1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ProductList from './ProductList'
-import Hero from '../components/HomePageCmponents/hero'
+import React from "react";
+import { Link } from "react-router-dom";
+import ProductList from "./ProductList";
+import Hero from "../components/HomePageCmponents/Hero";
+import AboutUsSection from "../components/AboutUsSectioncomponents/AboutUsSection";
+import Card from "../components/productCardcomponents/card";
 
 const HomePage = () => {
   return (
-    <div>
-      <Hero/>
+    <div className="flex flex-col gap-56">
+      <Hero />
+      <AboutUsSection />
+      <Card/>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
